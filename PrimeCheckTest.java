d53f2f9376dc6598e16eb37381896ad87068f05e
Aversity <toni.grafe01@gmail.com>
2023-06-30 09:19:23 +0200

    did some oopsies

--- PrimeCheckTest.java
+++ PrimeCheckTest.java
@@ -24,7 +24,7 @@ public class PrimeCheckTest {
   public void testPrimes() {
     int[] testListe = new int[]{3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67};
     for (int j = 0; j < testListe.length; j++) {
-      assertEquals("Test" + String(j+3), true, PrimeCheck.isPrime(testListe[j]));
+      assertEquals("Test" + Integer.toString(j+3), true, PrimeCheck.isPrime(testListe[j]));
     }
   }
 
@@ -33,7 +33,7 @@ public class PrimeCheckTest {
   public void testNonPrimes() {
     int[] testListe = new int[]{4, 6, 8, 9, 10, 12, 14, 15, 16, 18, 20, 21, 22, 24, 25, 26, 27, 28};
     for (int k = 0; k < testListe.length; k++ ) {
-      assertEquals("Test" + String(k+20), false, PrimeCheck.isPrime(testListe[k]));
+      assertEquals("Test" + Integer.toString(k+20), false, PrimeCheck.isPrime(testListe[k]));
     }
   }
 }
